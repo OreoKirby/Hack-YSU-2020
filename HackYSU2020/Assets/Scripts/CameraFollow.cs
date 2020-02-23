905e4d2b892c6163c3a52b3dbb9cb0c84e7dbec7
Michael Ciccone <hika567@gmail.com>
2020-02-23 04:50:00 -0500

    Updated CameraFollow Script

--- HackYSU2020/Assets/Scripts/CameraFollow.cs
+++ HackYSU2020/Assets/Scripts/CameraFollow.cs
@@ -7,23 +7,37 @@ public class CameraFollow : MonoBehaviour
 
     private GameObject player;
     private float _playerX;
+    private float startPlayerX;
+    private float startPlayerY;
+    private float startCameraY;
+    private float startCameraX;
+    private float startCameraZ;
     private float _playerY;
     private float _playerZ;
+    private float startPlayerZ;
     
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        var position = player.transform.position;
+        startPlayerX = position.x;
+        startPlayerZ = position.z;
+        startPlayerY = position.y;
+        startCameraX = gameObject.transform.position.x;
+        startCameraY = gameObject.transform.position.y;
+        startCameraZ = gameObject.transform.position.z;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         var position = player.transform.position;
         _playerX = position.x;
         _playerY = position.y;
         _playerZ = position.z;
         
-        this.gameObject.transform.position = new Vector3(_playerX, this.transform.position.z, _playerZ);
+        this.gameObject.transform.position = new Vector3(startCameraX + (_playerX - startPlayerX), startCameraY + (_playerY - startPlayerY), startCameraZ + (_playerZ - startPlayerZ));
+        //transform.LookAt(player.transform);
     }
 }
