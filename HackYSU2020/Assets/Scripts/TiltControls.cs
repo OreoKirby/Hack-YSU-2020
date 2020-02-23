58e7bd11e2cd260ec5d0d323cfe2080b5bb22d3f
Michael Ciccone <hika567@gmail.com>
2020-02-23 01:23:25 -0500

    Limited Rotation Angles of joycon

--- HackYSU2020/Assets/Scripts/TiltControls.cs
+++ HackYSU2020/Assets/Scripts/TiltControls.cs
@@ -8,16 +8,18 @@ public class TiltControls : MonoBehaviour
 
     public Vector3 gyro;
     public Vector3 accel;
+    public float xMax, yMax, zMax;
     public Quaternion orientation;
-    public Quaternion fix;
+    private Quaternion fix;
+    public float maxAngle;
 
-
+    private Quaternion max;
 
     // Start is called before the first frame update
     void Start()
     {
         gyro = new Vector3(0, 0, 0);
-        accel = new Vector3(0, 0, 0);
+        accel = new Vector3(0, 0, 0);      
         // get the public Joycon object attached to the JoyconManager in scene
         j = JoyconManager.Instance.j;
     }
@@ -36,8 +38,62 @@ public class TiltControls : MonoBehaviour
 
             gameObject.transform.rotation = orientation;
 
-            fix = Quaternion.Euler(this.transform.rotation.eulerAngles.y - 90, this.transform.rotation.eulerAngles.x, -this.transform.rotation.eulerAngles.z + 90);
+
+            //         if ((this.transform.rotation.eulerAngles.y - 90) < 180)
+            //               xCord = (this.transform.rotation.eulerAngles.y - 90);
+            //            else
+
+            //if (this.transform.rotation.eulerAngles.y < maxAngle && this.transform.rotation.eulerAngles.y > -maxAngle)
+            //{
+            //    yMax = this.transform.rotation.eulerAngles.x;
+            //}
+
+            if (this.transform.rotation.eulerAngles.y - 90 < maxAngle && this.transform.rotation.eulerAngles.y - 90 > -maxAngle)
+            {
+                xMax = this.transform.rotation.eulerAngles.y - 90;
+            }
+
+            if (-this.transform.rotation.eulerAngles.z + 90 < maxAngle && -this.transform.rotation.eulerAngles.z + 90 > -maxAngle)
+            {
+                zMax = -this.transform.rotation.eulerAngles.z + 90;
+            }
+
+            if (-this.transform.rotation.eulerAngles.x < maxAngle && -this.transform.rotation.eulerAngles.x > -maxAngle)
+            {
+                yMax = -this.transform.rotation.eulerAngles.x;
+            }
+
+            fix = Quaternion.Euler(xMax, yMax, zMax);
             gameObject.transform.rotation = fix;
+
+            //if (accel.y < maxAngle && accel.y > -maxAngle)
+            //{
+            //    yMax = this.transform.rotation.eulerAngles.x;
+            //}
+
+
+            //if (accel.x < maxAngle && accel.x > -maxAngle)
+            //{
+            //    xMax = this.transform.rotation.eulerAngles.y - 90;
+            //}
+
+
+
+
+
+
+            //if (accel.x < maxAngle && accel.x > -maxAngle)
+            //{
+
+            //fix = Quaternion.Euler(this.transform.rotation.eulerAngles.y - 90, this.transform.rotation.eulerAngles.x, -this.transform.rotation.eulerAngles.z + 90);
+            //gameObject.transform.rotation = fix;
+
+            //    max = fix;
+            //}
+            //else
+            //{
+            //    gameObject.transform.rotation = max;
+            //}
         }
     }
 }
