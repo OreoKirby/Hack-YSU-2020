fe729d17b004cd14f0911bb0080ce6d094347959
Michael Ciccone <hika567@gmail.com>
2020-02-23 01:55:51 -0500

    Restricted ability to rotate y-axis

--- HackYSU2020/Assets/Scripts/TiltControls.cs
+++ HackYSU2020/Assets/Scripts/TiltControls.cs
@@ -13,8 +13,6 @@ public class TiltControls : MonoBehaviour
     private Quaternion fix;
     public float maxAngle;
 
-    private Quaternion max;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -38,16 +36,6 @@ public class TiltControls : MonoBehaviour
 
             gameObject.transform.rotation = orientation;
 
-
-            //         if ((this.transform.rotation.eulerAngles.y - 90) < 180)
-            //               xCord = (this.transform.rotation.eulerAngles.y - 90);
-            //            else
-
-            //if (this.transform.rotation.eulerAngles.y < maxAngle && this.transform.rotation.eulerAngles.y > -maxAngle)
-            //{
-            //    yMax = this.transform.rotation.eulerAngles.x;
-            //}
-
             if (this.transform.rotation.eulerAngles.y - 90 < maxAngle && this.transform.rotation.eulerAngles.y - 90 > -maxAngle)
             {
                 xMax = this.transform.rotation.eulerAngles.y - 90;
@@ -58,7 +46,7 @@ public class TiltControls : MonoBehaviour
                 zMax = -this.transform.rotation.eulerAngles.z + 90;
             }
 
-            if (-this.transform.rotation.eulerAngles.x < maxAngle && -this.transform.rotation.eulerAngles.x > -maxAngle)
+            if (this.transform.rotation.eulerAngles.x < maxAngle && -this.transform.rotation.eulerAngles.x > maxAngle)
             {
                 yMax = this.transform.rotation.eulerAngles.x;
             }
@@ -66,34 +54,6 @@ public class TiltControls : MonoBehaviour
             fix = Quaternion.Euler(xMax, yMax, zMax);
             gameObject.transform.rotation = fix;
 
-            //if (accel.y < maxAngle && accel.y > -maxAngle)
-            //{
-            //    yMax = this.transform.rotation.eulerAngles.x;
-            //}
-
-
-            //if (accel.x < maxAngle && accel.x > -maxAngle)
-            //{
-            //    xMax = this.transform.rotation.eulerAngles.y - 90;
-            //}
-
-
-
-
-
-
-            //if (accel.x < maxAngle && accel.x > -maxAngle)
-            //{
-
-            //fix = Quaternion.Euler(this.transform.rotation.eulerAngles.y - 90, this.transform.rotation.eulerAngles.x, -this.transform.rotation.eulerAngles.z + 90);
-            //gameObject.transform.rotation = fix;
-
-            //    max = fix;
-            //}
-            //else
-            //{
-            //    gameObject.transform.rotation = max;
-            //}
         }
     }
 }
