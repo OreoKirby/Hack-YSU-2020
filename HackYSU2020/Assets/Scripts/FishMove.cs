d74f198984421b99715cbc578ecef66e58d2e9ce
Shayne <hardcorezeldafan@gmail.com>
2020-02-22 23:59:14 -0500

    Added Player and Board objects

---
+++ HackYSU2020/Assets/Scripts/FishMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMove : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private GameObject _bubble;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+        _bubble = GameObject.FindGameObjectWithTag("Bubble");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = _bubble.transform.position;
+        // Debug.Log(position);
+        _rb.transform.position = position;
+    }
+}
